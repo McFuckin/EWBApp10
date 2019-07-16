50fbe7d02d344847ab17297eda55eaf027efcbe6
JPMcGucki <jpmcgu2@gmail.com>
2019-07-16 19:50:55 -0400

    Update Week 4 - Josh's Tasks

---
+++ app/src/main/java/com/drexelewb/drexelewbapp/Info.java
@@ -0,0 +1,12 @@
+package com.drexelewb.drexelewbapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class Info extends AppCompatActivity {
+
+        private TextView Text;
+        private
+
+
+}
